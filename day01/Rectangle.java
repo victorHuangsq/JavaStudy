0870435649f67cf7d6322946efd987c5413e78be
victorHuangsq <hsq_encourage@163.com>
2015-03-24 14:40:09 +0800

    Init

---
+++ day01/Rectangle.java
@@ -0,0 +1,26 @@
+/***************************************************
+Class Name   : Rectangle
+Function Name:
+Description  : ´òÓ¡Èý½ÇÐÎ
+Input		 :
+Output		 :
+Notes        :
+Date		 : 2015-03-24
+Author       : victorHuangsq
+***************************************************/
+public class Rectangle{
+
+	public static void main(String[] argv){
+		int i = 0;
+		int j = 0;
+		System.out.println("Hello Java");
+		for(i=1; i<10; i++)
+		{
+			for(j=0; j<i; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+	}
+}
